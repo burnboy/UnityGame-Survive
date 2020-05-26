b1c761bedbdfe4917eda2b6a8dcd4ac5c7df7d31
burnboy <uty1993@naver.com>
2020-05-26 18:43:39 +0900

    플레이어 카메라 이동
    플레이어 카메라 이동

---
+++ Assets/02.Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed;    
+    //몸체 선언
+    [SerializeField]
+    private Rigidbody myRigid;
+    [SerializeField]
+    private Camera theCamera;
+
+    //카메라의 민감도
+    [SerializeField]
+    private float lookSensitivity;
+
+    [SerializeField]
+    private float cameraRotationLimit;
+    private float currentCameraRotation_X = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // theCamera = FindObjectOfType<Camera>();
+        myRigid = GetComponent<Rigidbody>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CameraRotation();
+    }
+
+    private void Move()
+    { 
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+    }
+
+    private void CameraRotation()
+    {
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotation_X = _xRotation * lookSensitivity;
+        currentCameraRotation_X += _cameraRotation_X;
+        currentCameraRotation_X = Mathf.Clamp(currentCameraRotation_X ,- cameraRotationLimit, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotation_X, 0f, 0f);
+    }
+}
