27a2206412f24392cf273e8ee31959a10b173561
burnboy <uty1993@naver.com>
2020-05-31 01:08:24 +0900

    달리기, 걷기 추가

--- Assets/02.Scripts/PlayerController.cs
+++ Assets/02.Scripts/PlayerController.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float walkSpeed;    
+    private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+    private float applySpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
+    private bool isRun = false;
+    private bool isGround = true;
+    private CapsuleCollider CapsuleCollider;
+
     //몸체 선언
     [SerializeField]
     private Rigidbody myRigid;
@@ -16,6 +27,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float lookSensitivity;
 
+    //카메라 한계
     [SerializeField]
     private float cameraRotationLimit;
     private float currentCameraRotation_X = 0f;
@@ -25,6 +37,8 @@ public class PlayerController : MonoBehaviour
     {
        // theCamera = FindObjectOfType<Camera>();
         myRigid = GetComponent<Rigidbody>();
+        //처음상태는 걷는상태
+        applySpeed = walkSpeed;
     }
 
 
@@ -32,9 +46,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        TryRun();
         Move();
         CameraRotation();
+        CharacterRotation();
     }
+
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+
+        }
+
+    }
+
+    private void Running()
+    {
+        isRun = true;
+        applySpeed = runSpeed;
+
+    }
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
 
     private void Move()
     { 
@@ -43,18 +87,28 @@ public class PlayerController : MonoBehaviour
 
         Vector3 _moveHorizontal = transform.right * _moveDirX;
         Vector3 _moveVertical = transform.forward * _moveDirZ;
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
+       // Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
 
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
     }
 
     private void CameraRotation()
     {
+        //위아래 회전
         float _xRotation = Input.GetAxisRaw("Mouse Y");
         float _cameraRotation_X = _xRotation * lookSensitivity;
-        currentCameraRotation_X += _cameraRotation_X;
+        currentCameraRotation_X -= _cameraRotation_X;
         currentCameraRotation_X = Mathf.Clamp(currentCameraRotation_X ,- cameraRotationLimit, cameraRotationLimit);
 
         theCamera.transform.localEulerAngles = new Vector3(currentCameraRotation_X, 0f, 0f);
+    }
+
+    private void CharacterRotation()
+    {
+        //좌우 캐릭터 회전
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0, _yRotation, 0) * lookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
     }
 }
